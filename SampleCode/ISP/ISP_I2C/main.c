775c5712cd0479765fcb4e40c4cb32921ac73f0b
SFWU <SFWU@nuvoton.com>
2019-09-25 10:04:51 +0800

    Add ISP_I2C sample code.
    Change-Id: Ifc126d7103be87453ae80ddf841e6d135f5f6e63

---
+++ SampleCode/ISP/ISP_I2C/main.c
@@ -0,0 +1,137 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "targetdev.h"
+#include "i2c_transfer.h"
+
+#define PLLCON_SETTING  CLK_PLLCON_50MHz_HIRC
+#define PLL_CLOCK       50000000
+
+uint32_t u32Pclk0;
+uint32_t u32Pclk1;
+
+void ProcessHardFault(void) {}
+void SH_Return(void) {}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable Internal RC 22.1184MHz clock */
+    CLK->PWRCON |= (CLK_PWRCON_OSC22M_EN_Msk | CLK_PWRCON_XTL12M_EN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    while (!(CLK->CLKSTATUS & CLK_CLKSTATUS_OSC22M_STB_Msk));
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK->PLLCON = PLLCON_SETTING;
+
+    /* Wait for PLL stable */
+    while (!(CLK->CLKSTATUS & CLK_CLKSTATUS_PLL_STB_Msk));
+
+    /* Set HCLK as PLL */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLK_S_Msk)) | CLK_CLKSEL0_HCLK_S_PLL;
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_HCLK_N_Msk)) | CLK_CLKDIV_HCLK(1);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
+    PllClock        = PLL_CLOCK;            // PLL
+    SystemCoreClock = PLL_CLOCK / 1;        // HCLK
+    CyclesPerUs     = PLL_CLOCK / 1000000;  // For SYS_SysTickDelay()
+
+    /* Enable I2C1 peripheral clock */
+    CLK->APBCLK |= CLK_APBCLK_I2C1_EN_Msk;
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and CyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set PA multi-function pins for I2C1 SDA and SCL */
+    SYS->GPA_MFP &= ~(SYS_GPA_MFP_PA10_Msk | SYS_GPA_MFP_PA11_Msk);
+    SYS->GPA_MFP |= (SYS_GPA_MFP_PA10_I2C1_SDA | SYS_GPA_MFP_PA11_I2C1_SCL);
+
+    /* I2C clock pin enable schmitt trigger */
+    SYS->GPA_MFP |= (11<<SYS_GPA_MFP_GPA_TYPE_Pos);
+}
+
+int main(void)
+{
+    uint32_t au8CmdBuff[16];
+
+    /* Unlock write-protected registers */
+    SYS_UnlockReg();
+
+    /* Configure WDT */
+    WDT->WTCR &= ~(WDT_WTCR_WTE_Msk | WDT_WTCR_DBGACK_WDT_Msk);
+    WDT->WTCR |= (WDT_TIMEOUT_2POW18 | WDT_WTCR_WTR_Msk);
+
+    /* Init system and multi-funcition I/O */
+    SYS_Init();
+
+    /* Enable FMC ISP */
+    FMC->ISPCON |= FMC_ISPCON_ISPEN_Msk;
+
+    /* Get APROM size, data flash size and address */
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+
+    /* Init I2C */
+    I2C_Init();
+
+    /* Set Systick time-out as 300ms */
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+
+    /* Wait for CMD_CONNECT command until Systick time-out */
+    while (1)
+    {
+        /* Wait for CMD_CONNECT command */
+        if (u8I2cDataReady == 1)
+        {
+            goto _ISP;
+        }
+        /* Systick time-out, go to APROM */
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    /* Parse command from master and send response back */
+    while (1)
+    {
+        if (u8I2cDataReady == 1)
+        {
+            /* Get command from I2C receive buffer */
+            memcpy(au8CmdBuff, au8I2cRcvBuf, 64);
+            u8I2cDataReady = 0;
+            /* Parse the current command */
+            ParseCmd((unsigned char *)au8CmdBuff, 64);
+        }
+    }
+
+_APROM:
+
+    /* Reset system and boot from APROM */
+    SYS->RSTSRC = (SYS_RSTSRC_RSTS_POR_Msk | SYS_RSTSRC_RSTS_RESET_Msk); /* Clear reset status flag */
+    FMC->ISPCON &= ~(FMC_ISPCON_BS_Msk|FMC_ISPCON_ISPEN_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
---
+++ SampleCode/ISP/ISP_I2C/targetdev.h
@@ -0,0 +1,13 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ *
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "NUC131.h"
+#include "i2c_transfer.h"
+#include "isp_user.h"
+
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
